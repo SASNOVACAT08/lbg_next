c355b1dff4c883cd20ece7a7992e3d060adf0caa
Ruben Barros <atosc08@gmail.com>
2021-01-15 11:56:17 +0100

    Event action & index

--- components/Event.js
+++ components/Event.js
@@ -1,6 +1,6 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
-export default function Event({token}){
+export default function Event({ token }) {
   const [events, setEvents] = useState([]);
   const [name, setName] = useState([]);
   const [isVisible, setIsVisible] = useState([]);
@@ -8,74 +8,74 @@ export default function Event({token}){
   const [timestamp, setTimeStamp] = useState([]);
 
   useEffect(async () => {
-    let data = await fetch ("http://localhost:4000/event",{
-      method:"GET",
-      headers:{ 
+    let data = await fetch("http://localhost:4000/event", {
+      method: "GET",
+      headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${token}`,
-      }, 
-    })
+      },
+    });
     let res = await data.json();
-    if(res.type === "success"){
+    if (res.type === "success") {
       setEvents(res.data);
     }
-  },
-  
-  );
-// async function postEvent(){
-//   let data = await fetch("http://localhost:4000/event", {
-//     method: "POST",
-//     headers: { "Content-Type": "application/json",
-//     Authorization: `Bearer ${token}`
-//    },
-  
-//     body: JSON.stringify({
-//       name,
-//       isVisible,
-//       isValid,
-//       timestamp,
-//     }),
-//   });
+  });
+  // async function postEvent(){
+  //   let data = await fetch("http://localhost:4000/event", {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/json",
+  //     Authorization: `Bearer ${token}`
+  //    },
 
-//   let res = await data.json();
-//   console.log(res);
-//   if (res.type === "success") {
-   
-//     console.log(res.data);
-//   }
-// }
- async function deleteEvent(id){
-  console.log(id);
-  // Similaire à componentDidMount et componentDidUpdate :
-  //useEffect(async () => {
-    try{
-      
-    let data = await fetch ("http://localhost:4000/event/"+id,{ 
-      method:"DELETE",
-      headers:{ 
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      }, 
-   // })
+  //     body: JSON.stringify({
+  //       name,
+  //       isVisible,
+  //       isValid,
+  //       timestamp,
+  //     }),
+  //   });
 
-  }); 
-}
-  catch{};
-  
-}
-    return (
-      <section id="events">
-        {events.map((event) => (
-          <div key={event.id}>
-            <p>{event.id}</p>
-            <p>{event.name}</p>
-            
-            <p>{event.isVisible ? "true" : "false"}</p>
-            <input value="supr" type="button" onClick={async () => {deleteEvent(event.id)}}/>
-          </div>
-        ))}
-        <h2>post</h2>
-        {/* <form>
+  //   let res = await data.json();
+  //   console.log(res);
+  //   if (res.type === "success") {
+
+  //     console.log(res.data);
+  //   }
+  // }
+  async function deleteEvent(id) {
+    console.log(id);
+    // Similaire à componentDidMount et componentDidUpdate :
+    //useEffect(async () => {
+    try {
+      let data = await fetch("http://localhost:4000/event/" + id, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        // })
+      });
+    } catch {}
+  }
+  return (
+    <section id="events">
+      {events.map((event) => (
+        <div key={event.id}>
+          <p>{event.id}</p>
+          <p>{event.name}</p>
+
+          <p>{event.isVisible ? "true" : "false"}</p>
+          <input
+            value="supr"
+            type="button"
+            onClick={async () => {
+              deleteEvent(event.id);
+            }}
+          />
+        </div>
+      ))}
+      <h2>post</h2>
+      {/* <form>
 
         <input
           type="text"
@@ -102,7 +102,6 @@ export default function Event({token}){
 
       <input type="button" value="add game" onClick={postEvent} />
       </form> */}
-         </section>
-    )
-  
-  }
+    </section>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,78 @@
-import Head from "next/head";
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return <div></div>;
+  const [hint, setHint] = useState([]);
+  const [sliderHint, setSliderHint] = useState([]);
+  const [day, setDay] = useState("00");
+  const [hour, setHour] = useState("00");
+  const [minute, setMinute] = useState("00");
+  const [second, setSecond] = useState("00");
+
+  useEffect(async () => {
+    const socket = io("http://localhost:4000/");
+    socket.on("RES_SEND_TIMER", (timer) => {
+      if (timer.date) {
+        let time = Date.parse(timer.date) - Date.now();
+        setInterval(() => {
+          time -= 1000;
+          let delta = Math.abs(time) / 1000;
+          let days = Math.floor(delta / 86400);
+          delta -= days * 86400;
+          let hours = Math.floor(delta / 3600) % 24;
+          delta -= hours * 3600;
+          let minutes = Math.floor(delta / 60) % 60;
+          delta -= minutes * 60;
+          let seconds = Math.round(delta % 60);
+          setDay(days);
+          setHour(hours);
+          setMinute(minutes);
+          setSecond(seconds);
+        }, 1000);
+      }
+    });
+    socket.on("RES_NEW_EVENT", async () => {
+      await fetchAll();
+    });
+    await fetchAll();
+  }, []);
+
+  async function fetchAll() {
+    let data = await fetch("http://localhost:4000/public/hint", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
+    let res = await data.json();
+    setHint(res.data);
+    data = await fetch("http://localhost:4000/public/sliderhint", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
+    res = await data.json();
+    setSliderHint(res.data);
+  }
+
+  return (
+    <section>
+      {`${day} : ${hour} : ${minute} : ${second}`}
+      <h2>Hint</h2>
+      <article>
+        {hint.map((hin) => (
+          <div key={hin.id}>
+            <p>{hin.name}</p>
+            <p>{hin.content}</p>
+          </div>
+        ))}
+      </article>
+      <h2>Slider Hint</h2>
+      <article>
+        {sliderHint.map((slh) => (
+          <div key={slh.id}>
+            <p>{slh.name}</p>
+            <p>{slh.link}</p>
+          </div>
+        ))}
+      </article>
+    </section>
+  );
 }
