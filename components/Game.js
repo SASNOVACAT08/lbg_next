19120c81ea4f252b11bbbfff9f8e6e83166f1c0f
= <=>
2021-01-15 10:18:06 +0100

    put added to hint

--- components/Game.js
+++ components/Game.js
@@ -2,6 +2,7 @@ import {useEffect, useState} from "react";
 
 export default function Game({token}){
   const [games, setGames ] = useState([]);
+  const [gameId, setGameId]  = useState("New game");
   const [name, setName]  = useState("New game");
   const [isVisible, setIsVisible]  = useState(false);
 
@@ -31,14 +32,25 @@ export default function Game({token}){
   
 }
 async function putGame(){
-
-      let data = await fetch ("http://localhost:4000/game/"+id,{ 
+      console.log("ok");
+      let data = await fetch ("http://localhost:4000/game/"+gameId,{ 
       method:"PUT",
       headers:{ 
         "Content-Type": "application/json",
         Authorization: `Bearer ${token}`,
       }, 
+      body: JSON.stringify({
+        name,
+        isVisible,
+        gameId
+      })
   }); 
+  let res = await data.json();
+  console.log(res);
+  if (res.type === "success") {
+   
+    console.log(res.data);
+  }
 }
 async function postGame(){
   let data = await fetch("http://localhost:4000/game", {
@@ -63,18 +75,22 @@ async function postGame(){
 
     return (
       <section id="games">
+        <h2>List of the games</h2>
         {games.map((game) => (
           <div key={game.id}>
-            <p>{game.id}</p>
-            <p>{game.name}</p>
-            <p>{game.isVisible ? "true" : "false"}</p>
+            <p>id :{game.id}</p>
+            <p>name : {game.name}</p>
+            <p>is visible ? {game.isVisible ? "true" : "false"}</p>
             <hr/> 
-            <h2>Delete</h2> 
+          
             <input value="supr" type="button" onClick={async () => {deleteGame(game.id)}}/>
             <hr/> 
-            <h2>Post</h2> 
+          </div>
+        ))}
+         
+            <h2>Post a game</h2> 
             <form>
-        <input
+            <input
           type="text"
           value={name}
           name="name"
@@ -86,13 +102,36 @@ async function postGame(){
           value={isVisible}
           name="isVisible"
           placeholder="isVisible"
-          onChange={({ target: { value } }) => setIsVisible(value == "true")  }
-        />
-      <input type="button" value="add game" onClick={postGame} />
+          onChange={({ target: { value } }) => setIsVisible(value == "true")  }/>
+      <input type="button" 
+        value="add game" 
+        onClick={postGame} />
+
       </form>
-           
-          </div>
-        ))}
+    <h2>Modify game</h2>
+      <form>
+      <input
+          type="text"
+          value={name}
+          name="name"
+          placeholder="name of the game"
+          onChange={({ target: { value } }) => setName(value)}/>
+
+          <input
+            type="text"
+            value={gameId}
+            name="gameId"
+            placeholder="gameId"
+            onChange={({ target: { value } }) => setGameId(parseInt(value))  }/>
+
+        <input
+          type="checkbox"
+          value={isVisible}
+          name="isVisible"
+          placeholder="isVisible"
+          onChange={({ target: { value } }) => setIsVisible(value == "true")  }/>
+          <input type="button" value="add game" onClick={putGame} />
+      </form>
          </section>
     )
   
